aa810c4ceea90a08463f0d5d50d15dcf0eeef5ea
Kostya <kostya_bogd@mail.ru>
2026-04-02 15:20:45 +0500

    Добавил движение НПС

--- Assets/Scripts/NPC/NPCScript.cs
+++ Assets/Scripts/NPC/NPCScript.cs
@@ -1,15 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPCScript : MonoBehaviour
 {
 
     public float Hp = 100f;
     private GameObject player;
+    private Transform playerTransform;
+    private NavMeshAgent navMeshAgent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player=GameObject.FindGameObjectWithTag("Player");
+        navMeshAgent=GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
     }
 
     // Update is called once per frame
@@ -19,7 +25,7 @@ public class NPCScript : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
+        navMeshAgent.SetDestination(player.transform.position);
         if (player.transform.position.x < transform.position.x &&  transform.localScale.x>0)
         {
             Flip(true);
