a5cafe94ddf60426a5314fbef5a516159b4dc68e
Kostya <kostya_bogd@mail.ru>
2026-04-02 16:26:58 +0500

    .

--- Assets/Scripts/NPC/NPCScript.cs
+++ Assets/Scripts/NPC/NPCScript.cs
@@ -4,16 +4,17 @@ using UnityEngine.AI;
 
 public class NPCScript : MonoBehaviour
 {
-
     public float Hp = 100f;
     private GameObject player;
     private Transform playerTransform;
+
     private NavMeshAgent navMeshAgent;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        player=GameObject.FindGameObjectWithTag("Player");
-        navMeshAgent=GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.updateRotation = false;
         navMeshAgent.updateUpAxis = false;
     }
@@ -25,8 +26,9 @@ public class NPCScript : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
         navMeshAgent.SetDestination(player.transform.position);
-        if (player.transform.position.x < transform.position.x &&  transform.localScale.x>0)
+        if (player.transform.position.x < transform.position.x && transform.localScale.x > 0)
         {
             Flip(true);
         }
@@ -35,6 +37,7 @@ public class NPCScript : MonoBehaviour
             Flip(false);
         }
     }
+
     void Flip(bool flip)
     {
         if (flip)
