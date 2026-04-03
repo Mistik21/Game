2174a889fb2ce38c21a28c34642d16ba2087a8a1
Kostya <kostya_bogd@mail.ru>
2026-04-04 01:35:39 +0500

    Добавил стартовое меню

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Метод для кнопки "Начать игру"
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene"); // Укажите имя вашей игровой сцены
+    }
+    public void ExitGame()
+    {
+        
+#if UNITY_EDITOR
+        // Если в редакторе Unity - останавливаем игру
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // Если в собранной игре - закрываем приложение
+            Application.Quit();
+#endif
+    }
+}
