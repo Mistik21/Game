d33f35cd49223e6d74f04c70d3b43c67a657fd0e
Kostya <kostya_bogd@mail.ru>
2026-05-02 20:12:23 +0500

    комната спавна механика перехода между уровнями

---
+++ Assets/Scripts/Room/MainSpavn/StartGameSpavn.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class StartGameSpavn : MonoBehaviour
+{
+    private bool isPlayerInside = false;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInside = true;
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInside = false;
+        }
+    }
+    
+    void Update()
+    {
+        if (isPlayerInside && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            var user = GameObject.FindWithTag("Player");
+            user.GetComponent<TransferPlayer>().enabled=true;
+            foreach (Transform child in user.transform)
+            {
+                if (child.CompareTag("Light"))
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
